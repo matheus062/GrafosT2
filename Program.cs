aa77a408c840c0dea5f63b84cb82465802e34141
MatheusSilvestre <matheussilvestre3@gmail.com>
2023-04-14 02:35:34 -0300

    Corrige leitura de arquivo de Grafo.

--- Program.cs
+++ Program.cs
@@ -8,11 +8,11 @@ Console.WriteLine("\r");
 
 GraphList graphList = new(false, true);
 
-graphList.NodeInsert("A");
-graphList.NodeInsert("B");
-graphList.NodeInsert("C");
-graphList.NodeInsert("D");
-graphList.NodeInsert("E");
+//graphList.NodeInsert("A");
+//graphList.NodeInsert("B");
+//graphList.NodeInsert("C");
+//graphList.NodeInsert("D");
+//graphList.NodeInsert("E");
 
 //graphList.EdgeInsert(0, 2, 5);
 //graphList.EdgeInsert(0, 3, 4);
@@ -22,15 +22,15 @@ graphList.NodeInsert("E");
 //graphList.EdgeInsert(1, 4, 10);
 //graphList.EdgeInsert(1, 3, -2);
 
-graphList.EdgeInsert(0, 1, 3);
-graphList.EdgeInsert(0, 2, 5);
-graphList.EdgeInsert(0, 3, 6);
-graphList.EdgeInsert(0, 4, 8);
-graphList.EdgeInsert(1, 3, 2);
-graphList.EdgeInsert(1, 4, 11);
-graphList.EdgeInsert(2, 4, 2);
+//graphList.EdgeInsert(0, 1, 3);
+//graphList.EdgeInsert(0, 2, 5);
+//graphList.EdgeInsert(0, 3, 6);
+//graphList.EdgeInsert(0, 4, 8);
+//graphList.EdgeInsert(1, 3, 2);
+//graphList.EdgeInsert(1, 4, 11);
+//graphList.EdgeInsert(2, 4, 2);
 
-//graphList.LoadFile("C:\\Users\\natan\\source\\repos\\GrafosT2\\graph.txt");
+graphList.LoadFile(System.Environment.CurrentDirectory + "/graph.txt");
 graphList.GraphPrint();
 
 
